e8fa7b29f5b153974b3bc332313bfa4594810f12
Alex Pakalniskis <alexpakalniskis3@gmail.com>
2020-04-27 16:15:06 -0700

    Fix path error

--- web_app/services/strain_service.py
+++ web_app/services/strain_service.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-url_strains = "https://raw.githubusercontent.com/bw-ft-medcab3-brian/ds/master/schema_json.txt"
+url_strains = "https://raw.githubusercontent.com/bw-ft-medcab3-brian/ds/master/schemas/json_example_schema.txt"
 result = requests.get(url_strains)
 strains = json.loads(result.text)
 
