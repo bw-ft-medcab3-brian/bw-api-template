1781a19d2500723417a08fc18e66ce7b542cf325
tmbern <tmbern42@gmail.com>
2020-04-27 12:12:20 -0600

    update recommendation route with dummy data

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -1,13 +1,13 @@
 # web_app/routes/home_routes.py
 
 from flask import Blueprint
+from web_app.models import Strain, db, parse_records
 
 home_routes = Blueprint("home_routes", __name__)
 
-@home_routes.route("/")
+@home_routes.route("/strains.json")
 def index():
-    return "Cannabis Strain Recommendation API"
-
-@home_routes.route("/about")
-def about():
-    return "About this API"
+    def list_games():
+    strain_records = Strain.query.all()
+    strains = parse_records(strain_records)
+    return jsonify(strains)
--- web_app/routes/recommendation_routes.py
+++ web_app/routes/recommendation_routes.py
@@ -7,8 +7,23 @@ recommendation_routes = Blueprint("recommendation_routes", __name__)
 @recommendation_routes.route("/recommendations.json")
 def list_recommendations():
     recommendations = [
-        {"id": 1, "title": "Recommendation 1"},
-        {"id": 2, "title": "Recommendation 2"},
-        {"id": 3, "title": "Recommendation 3"},
-    ]
+        {"id": 1, "strain_name": "Recommendation 1", "strain_type": "hybrid",
+         "strain_terpene_profile": "profile type 1", "strain_flavor_profile": "flavor_1",
+         "strain_frangrance_profile": "fragrance", "strain_review_key": "review_1",
+         "strain_nearest_neighbors": "nearest_1", "strain_description": "description_1",
+         "strain_image": "image_1" },
+
+         {"id": 2, "strain_name": "Recommendation 2", "strain_type": "sativa",
+         "strain_terpene_profile": "profile type 2", "strain_flavor_profile": "flavor_2",
+         "strain_frangrance_profile": "fragrance_2", "strain_review_key": "review_2",
+         "strain_nearest_neighbors": "nearest_2", "strain_description": "description_2",
+         "strain_image": "image_2" },
+        
+         {"id": 3, "strain_name": "Recommendation 3", "strain_type": "sativa",
+         "strain_terpene_profile": "profile type 3", "strain_flavor_profile": "flavor_3",
+         "strain_frangrance_profile": "fragrance_3", "strain_review_key": "review_3",
+         "strain_nearest_neighbors": "nearest_3", "strain_description": "description_3",
+         "strain_image": "image_3" },        
+
+    # TODO return ML recommendation using pickle object.
     return jsonify(recommendations)
